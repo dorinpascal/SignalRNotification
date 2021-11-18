b14dbab5296a5ce4a0d5e29e0887226667081ec2
dorinpascal <74955991+dorinpascal@users.noreply.github.com>
2021-11-18 23:26:37 +0100

    //Pascal 11/18/2021

---
+++ SignalRNotification/Data/ChatHub.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRNotification.Data
+{
+    public class ChatHub :Hub
+    {
+        public async Task sendMessage(string sender,string receiver,string msgTitle,string msgBody)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",sender,receiver,msgTitle,msgBody);
+            
+        }
+    }
+}
---
+++ SignalRNotification/Data/Notification.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace SignalRNotification.Data
+{
+    public class Notification
+    {
+        public int MsgId { get; set; } = 0;
+        public string SenderName { get; set; } = "";
+        public string ReciverName { get; set; } = "";
+        public string MsgTitle { get; set; } = "";
+        public string MsgBody { get; set; } = "";
+        public DateTime MsgDate { get; set; } = DateTime.Now;
+        public string MsgDateSt => this.MsgDate.ToString("dd-MMM-yyyy");
+
+
+    }
+}
